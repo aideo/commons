9717fda6fd8ae34675ce0c927986aa9d1a422817
Kouichi Machida <k01@white-wing.jp>
2018-04-13 23:32:07 +0900

    Escaped String をデコードする設定を追加。

--- src/Utilities/PhpOffice/PHPExcelUtils.php
+++ src/Utilities/PhpOffice/PHPExcelUtils.php
@@ -18,21 +18,51 @@ class PHPExcelUtils
 {
 
     /**
+     * 返却値を trim する事を表します。
+     */
+    const OPT_TRIM = 1;
+
+    /**
+     * Escaped String を戻す事を表します。
+     */
+    const OPT_UNESCAPED = 2;
+
+    private static $escapedStringFrom = [];
+
+    private static $escapedStringTo = [];
+
+    private static $initializedEscapedString = false;
+
+    private static function filterOpt($value, $options)
+    {
+        if (in_array(self::OPT_UNESCAPED, $options, true)) {
+            $value = self::unescapedString($value);
+        }
+
+        if (in_array(self::OPT_TRIM, $options, true)) {
+            $value = StringUtils::trim($value);
+        }
+
+        return $value;
+    }
+
+    /**
      * ワークシートの列・行を指定してセルの値を取得します。
      *
      * @param PHPExcel_Worksheet $sheet ワークシート。
      * @param int|string $columnIndex 列番号。
      * @param int|string $rowIndex 行番号。
      * @param string|null $defaultValue セルが有効ではない場合のデフォルト値。
+     * @param array $options オプション。
      *
      * @return null|string セルの値。
      */
-    public static function getCellValue(PHPExcel_Worksheet $sheet, $columnIndex = 0, $rowIndex = 1, $defaultValue = null)
+    public static function getCellValue(PHPExcel_Worksheet $sheet, $columnIndex = 0, $rowIndex = 1, $defaultValue = null, $options = [self::OPT_TRIM, self::OPT_UNESCAPED])
     {
         if ($sheet->cellExistsByColumnAndRow($columnIndex, $rowIndex)) {
             $cell = $sheet->getCellByColumnAndRow($columnIndex, $rowIndex);
 
-            return StringUtils::trim($cell->getFormattedValue());
+            return self::filterOpt($cell->getFormattedValue(), $options);
         } else {
             return $defaultValue;
         }
@@ -44,18 +74,19 @@ class PHPExcelUtils
      * @param PHPExcel_Worksheet $sheet ワークシート。
      * @param string $name 名前付きセルの名前。
      * @param string|null $defaultValue 名前付きセルが有効ではない場合のデフォルト値。
+     * @param array $options オプション。
      *
      * @return null|string セルの値。
      * @throws PHPExcel_Exception セルの取得時に例外が発生した場合。
      */
-    public static function getCellValueByName(PHPExcel_Worksheet $sheet, $name, $defaultValue = null)
+    public static function getCellValueByName(PHPExcel_Worksheet $sheet, $name, $defaultValue = null, $options = [self::OPT_TRIM, self::OPT_UNESCAPED])
     {
         $namedRange = PHPExcel_NamedRange::resolveRange($name, $sheet);
 
         if ($namedRange !== null && $sheet->cellExists(($range = $namedRange->getRange()))) {
             $cell = $sheet->getCell($range);
 
-            return StringUtils::trim($cell->getFormattedValue());
+            return self::filterOpt($cell->getFormattedValue(), $options);
         } else {
             return $defaultValue;
         }
@@ -96,6 +127,26 @@ class PHPExcelUtils
     }
 
     /**
+     * Escaped String の変換マップを初期化します。
+     *
+     * https://msdn.microsoft.com/en-us/library/ff534667(v=office.12).aspx
+     */
+    private static function initializeEscapedString()
+    {
+        if (self::$initializedEscapedString === false) {
+            for ($i = 0; $i <= 31; ++$i) {
+                if ($i != 9 && $i != 10 && $i != 13) {
+                    $key = sprintf('_x%04s_', strtoupper(dechex($i)));
+                    $value = chr($i);
+
+                    self::$escapedStringFrom[] = $key;
+                    self::$escapedStringTo[] = $value;
+                }
+            }
+        }
+    }
+
+    /**
      * 対象シートの名前付きセルの定義をクリアします。
      *
      * @param PHPExcel $workbook 対象のワークブック。
@@ -132,6 +183,19 @@ class PHPExcelUtils
 
         $cell = $sheet->getCellByColumnAndRow($columnIndex, $rowIndex);
         $cell->setValue($value);
+    }
+
+    /**
+     * Escaped String を戻します。
+     *
+     * @param string $s 対象の文字列。
+     * @return string 返還後の文字列。
+     */
+    public static function unescapedString($s)
+    {
+        self::initializeEscapedString();
+
+        return str_replace(self::$escapedStringTo, self::$escapedStringFrom, $s);
     }
 
     /**
